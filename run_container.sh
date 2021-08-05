f0c342f8ef993210f18aec95b077450dd67b17fc
tomolopolis <tsearle88@gmail.com>
2021-08-05 14:32:57 +0100

    t5 config and outputs dir for trained models

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t -d --name mimic_sum_jupyter -v "$(pwd)"/../mimic_sum_data/:/app -p 9000:9000 tsearle/mimic_sum:latest jupyter_start.sh
+docker run -t -d -v "$(pwd)"/../mimic_sum_data/:/app -p 9000:9000 tsearle/mimic_sum:latest jupyter_start.sh
